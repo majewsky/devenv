d6aeb766d0b2a486226124ddba5b80df4f011ebb
Stefan Majewsky <stefan.majewsky@sap.com>
2015-11-25 14:06:46 +0100

    env_for_cloud.rb: make OS_PASSWORD optional

--- env_for_cloud.rb
+++ env_for_cloud.rb
@@ -4,7 +4,7 @@
 # shell function in my .profile, it's used like this:
 #
 #   $ env | grep OS_AUTH_URL
-#   $ cloud devstack
+#   $ cloud_is devstack
 #   $ env | grep OS_AUTH_URL
 #   OS_AUTH_URL=http://devstack.domain:5000/v3
 
@@ -14,11 +14,11 @@ require 'yaml'
 cloud_key = ARGV.first \
   or raise ArgumentError, "Usage: env_for_cloud.rb $CLOUD_NAME"
 cloud = YAML.load_file('/home/stefan/.config/openstack/clouds.yaml')['clouds'][cloud_key] \
-  or raise ArgumentError, "unknown cloud #{cloud}"
+  or raise ArgumentError, "unknown cloud #{cloud_key}"
 auth = cloud['auth']
 
 # some variables are always set (Identity v3 is always implied)
-  set_vars = %w[auth_url username password user_domain_name]
+  set_vars = %w[auth_url username user_domain_name]
 # domain/project/user IDs are not supported yet
 unset_vars = %w[domain_id user_domain_id project_domain_id project_id]
 
@@ -31,6 +31,13 @@ elsif auth.has_key?('project_name')
   unset_vars += %w[domain_name]
 else
   unset_vars += %w[project_name project_domain_name domain_name]
+end
+
+# is password given?
+if auth.has_key?('password')
+  set_vars += %w[password]
+else
+  unset_vars += %w[password]
 end
 
 # generate sh code
