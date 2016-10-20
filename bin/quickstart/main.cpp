75dc9e49250b12a8cf3a6aa39af20bffd9f08c49
Stefan Majewsky <majewsky@gmx.net>
2016-10-20 20:06:47 +0200

    quickstart: button styling

--- bin/quickstart/main.cpp
+++ bin/quickstart/main.cpp
@@ -7,7 +7,9 @@ int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
     app.setStyleSheet(QStringLiteral(
-        "QPushButton { font-size: 24px }"
+        "QPushButton { font-size: 24px; border: none; color:white; padding: 12px }"
+        "QPushButton:hover, QPushButton:pressed { background: #666 }"
+        "QPushButton:pressed { background: #CCC; color: black }"
     ));
 
     View view;
